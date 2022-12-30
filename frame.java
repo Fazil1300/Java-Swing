1ea0d27da056aad92d15ef23c42181a2e0603f72
Fazil134 <mrstark134@gmail.com>
2022-12-30 11:32:03 +0530

    JLabel Implemtations

--- frame.java
+++ frame.java
@@ -1,9 +1,12 @@
+import java.awt.Color;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 class frame{
     public static void main(String[] args) {
         ImageIcon icon=new ImageIcon("computer.png");//create icon
+
         //JFrame-a GUI window to add components
         JFrame frame=new JFrame();//creates frame
         frame.setVisible(true);//set frame visible
@@ -12,5 +15,6 @@ class frame{
         frame.setTitle("First Frame");//set title for frame
         frame.setResizable(false);//prevent frame being resizeable
         frame.setIconImage(icon.getImage());//set icon for frame
+        frame.getContentPane().setBackground(new Color(124,50,250));//change background color of frame
     }
 }
---
+++ label.java
@@ -0,0 +1,45 @@
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
+
+public class label {
+    public static void main(String[] args) {
+        //JLabel-a GUI display area for String of text/an image/both
+        ImageIcon lableIcon=new ImageIcon("dude.png");
+
+        Border border=BorderFactory.createLineBorder(Color.green, 3, false);//creates border
+
+        JLabel label=new JLabel();//creates label
+        label.setText("How's day dude?");//set text for label
+        label.setIcon(lableIcon);//set icon to label
+        label.setHorizontalTextPosition(JLabel.CENTER);//set text for Left,Center,Right of image
+        label.setVerticalTextPosition(JLabel.TOP);//set text for Top,Center,Bottom of image
+        label.setForeground(Color.YELLOW);//Change font color
+        label.setFont(new Font("MV BOli",Font.PLAIN,20));//Change font style,format,size
+        label.setIconTextGap(0);//Sets gap between text and image
+        label.setBackground(Color.black);//change background of label
+        label.setOpaque(true);//make background color visible
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.CENTER);//set vertical positon text+image within label
+        label.setHorizontalAlignment(JLabel.CENTER);//set horizontal text+image within label
+        label.setBounds(100, 100, 250, 250);//set label whereever you want within frame->make layoutManager to null 
+
+        JFrame frame=new JFrame();
+        frame.setVisible(true);
+        frame.setSize(500,500);
+        frame.setTitle("Lable");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(new Color(124,50,250));
+        frame.setResizable(false);
+        frame.setLayout(null);//make layout manager to null->for bounds
+        //frame.pack();//resize the frame to accomodate all the components we have
+        
+        frame.add(label);//adds label to frame
+    }
+}
+//Set the setResizeable,setLayout,setBounds to comment for pack methods 
